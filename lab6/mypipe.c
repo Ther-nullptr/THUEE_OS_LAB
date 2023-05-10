3aeede6443d33eb2a395442ae1f0a3bc705e6d40
ther <1329438302@qq.com>
2023-05-10 05:08:47 -0700

    chore(buffer): modify some buffer settings

--- lab6/mypipe.c
+++ lab6/mypipe.c
@@ -5,8 +5,8 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-#define PIPE_BUFFER_SIZE 4096
-#define PIPE_NUMBER 238
+#define PIPE_BUFFER_SIZE 16
+#define PIPE_NUMBER 200
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
@@ -14,14 +14,13 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ther-nullptr");
 MODULE_DESCRIPTION("a pipe");
 
-static int flag; // to detect whether the buffer is full or empty 
+static int flag = 0; // whether the buffer is empty or full
 static ssize_t p_read; // the pointer to read
 static ssize_t p_write; // the pointer to write
 static char* kernel_buffer; // the buffer in kernel space
 
 static struct mutex mutex_buffer;
-static struct semaphore sem_full;
-static struct semaphore sem_empty;
+static struct semaphore sem;
 
 static ssize_t mypipe_read(struct file *file, char __user *buf, size_t count, loff_t *f_pos)
 {
@@ -29,11 +28,13 @@ static ssize_t mypipe_read(struct file *file, char __user *buf, size_t count, lo
     mutex_lock_killable(&mutex_buffer);
     ssize_t actual_read_length = 0;
     int ret = 0;
-    while (flag == 0 && p_read == p_write) // wait until the buffer is readable
+    down_interruptible(&sem);
+    if (p_read == p_write && flag == 0)
     {
-        // block the read process
-        down_interruptible(&sem_empty);
+        printk(KERN_WARNING":the buffer is empty and will not be readable until the next write");
+        goto end_read;
     }
+    
     if (p_read < p_write)
     {
         actual_read_length = min(count, p_write - p_read); // the actual length to read is limited by the length of the buffer
@@ -53,16 +54,22 @@ static ssize_t mypipe_read(struct file *file, char __user *buf, size_t count, lo
             ret |= copy_to_user(buf + max_no_iterable, kernel_buffer, actual_read_length - max_no_iterable);
         }
     }
+    
+    printk(KERN_INFO":read %zu bytes\n", actual_read_length);
+    printk(KERN_INFO":p_read before %zu\n", p_read);
     p_read = (p_read + actual_read_length) % PIPE_BUFFER_SIZE;
-    flag = 0; // buffer will become empty after reading
+    printk(KERN_INFO":change p_read to %zu\n", p_read);
+    
     // wake up the write process
-    up(&sem_full);
+end_read:
+    up(&sem);
+    flag = 0;
+    
     if (ret != 0)
     {
-        printk(KERN_ALERT "Error in reading from pipe.\n");
+        printk(KERN_ALERT"Error in reading from pipe.\n");
         return -EFAULT;
     }
-
     mutex_unlock(&mutex_buffer);
     return actual_read_length;
 }
@@ -73,10 +80,14 @@ static ssize_t mypipe_write(struct file *file, const char __user *buf, size_t co
     mutex_lock_killable(&mutex_buffer);
     ssize_t actual_write_length = 0;
     int ret = 0;
-    while (flag == 1 && p_read == p_write) // wait until the buffer is writable
+    down_interruptible(&sem);
+    if (p_read == p_write && flag == 1)
     {
-        down_interruptible(&sem_full);
+        printk(KERN_WARNING":the buffer is full and will not be writeable until the next read");
+        actual_write_length = PIPE_BUFFER_SIZE;
+        goto end_write;
     }
+    
     if (p_read > p_write)
     {
         actual_write_length = min(count, p_read - p_write);
@@ -96,13 +107,20 @@ static ssize_t mypipe_write(struct file *file, const char __user *buf, size_t co
             ret |= copy_from_user(kernel_buffer, buf + max_no_iterable, actual_write_length - max_no_iterable);
         }
     }
+    
+    printk(KERN_INFO":write %zu bytes\n", actual_write_length);
+    printk(KERN_INFO":p_write before %zu\n", p_write);
     p_write = (p_write + actual_write_length) % PIPE_BUFFER_SIZE;
-    flag = 1; // buffer will become full after writing
+    printk(KERN_INFO":change p_write to %zu\n", p_write);
+
+end_write:
     // wake up the read process
-    up(&sem_empty);
+    up(&sem);
+    flag = 1;
+    
     if (ret != 0)
     {
-        printk(KERN_INFO "Error in writing to pipe.\n");
+        printk(KERN_INFO":Error in writing to pipe.\n");
     }
     mutex_unlock(&mutex_buffer);
     return actual_write_length;
@@ -132,9 +150,9 @@ static int __init mypipe_init(void)
     int ret;
     ret = register_chrdev(PIPE_NUMBER, "mypipe", &mypipe_fops);
     kernel_buffer = kmalloc(PIPE_BUFFER_SIZE, GFP_KERNEL);
-    sema_init(&sem_full, 0);
-    sema_init(&sem_empty, 1);
-    printk(KERN_INFO "mypipe: register device successfully\n");
+    mutex_init(&mutex_buffer);
+    sema_init(&sem, 1);
+    printk(KERN_INFO":mypipe register successfully\n");
     return 0;
 }
 
@@ -142,7 +160,9 @@ static void __exit mypipe_exit(void)
 {
     unregister_chrdev(PIPE_NUMBER, "mypipe");
     kfree(kernel_buffer);
-    printk(KERN_INFO "mypipe: unregister device successfully\n");
+    sema_init(&sem, 0);
+    mutex_destroy(&mutex_buffer);
+    printk(KERN_INFO":mypipe unregister successfully\n");
 }
 
 module_init(mypipe_init);
